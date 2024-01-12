f6b0ca6b6945f0b238c9330c528c9a16f4ffa1eb
Iker López <iker.lopezar@opendeusto.es>
2024-01-12 18:29:32 +0100

    RegisterPage and LoginPage

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -35,72 +35,79 @@ const LoginPage = () => {
     handleLogin(username, password);
   };
 
+  const handleNavigate = () => {
+    navigate("/register");
+  };
+
   return (
     <div className="login">
-      <img className="sits-01-icon" alt="" src="/sits-01@2x.png" />
-      <img
-        className="logo-combined-191a1e-icon"
-        alt=""
-        src="/logo-combined-191a1e@2x.png"
-      />
-
-      <div className="frame1">
-        <div className="frame1-1">
-          <h1 className="frame1-1-inicia-sesion">Inicia sesión</h1>
-          <h1 className="frame1-1-con-tu-nombre-de-usua">
-            con tu nombre de usuario
-          </h1>
-        </div>
-        <div className="frame1-2">
-          <div className="frame1-2-si-no-tienes-una-cuen">
-            Si no tienes una cuenta regístrate
+      <div className="frame2">
+        <div className="frame2-1">
+          <div className="frame2-1-1">
+            <h1 className="frame2-1-1-text1">Inicia sesión</h1>
+            <h1 className="frame2-1-1-text2">con tu nombre de usuario</h1>
           </div>
-          <a className="frame1-2-puedes-registrarte-aq">
-            <span>
-              <span>¡Puedes</span>
-              <span className="span">{` `}</span>
-            </span>
-            <span className="regstrarte-aqu">regístrarte aquí!</span>
-          </a>
+          <div className="frame2-1-2">
+            <h1 className="frame2-1-2-text1">
+              Si no tienes una cuenta regístrate
+            </h1>
+            <a className="frame2-1-2-text2" onClick={handleNavigate}>
+              <span>
+                <span>¡Puedes</span>
+                <span className="span">{` `}</span>
+              </span>
+              <span className="registrarte-aqu">registrarte aquí!</span>
+            </a>
+          </div>
         </div>
-      </div>
-
-      {/* Formulario de inicio de sesión */}
-      <form onSubmit={handleSubmit} className="frame2">
-        <h1 className="frame2-inicia-sesion">Inicia sesión</h1>
-        <div className="frame2-group1">
-          <input
-            className="frame2-group1-1"
-            placeholder="Nombre de usuario"
-            type="text"
-            value={username}
-            onChange={(event) => setUsername(event.target.value)}
-          />
-          {/*<input
-            className="group-item"
+        <div className="frame2-2">
+          <h1 className="frame2-2-text">Inicia sesión</h1>
+          {/* Formulario de inicio de sesión */}
+          <form className="frame2-2-1" onSubmit={handleSubmit}>
+            <div className="frame2-2-1-1">
+              <input
+                className="frame2-2-1-1-1"
+                placeholder="Nombre de usuario"
+                type="text"
+                value={username}
+                onChange={(event) => setUsername(event.target.value)}
+              />
+              {/*<input
+            className="frame2-2-1-1-1"
             placeholder="Correo electrónico"
             type="email"
-            value={groupInput1Value}
+            value={email}
             onChange={(event) => setGroupInput1Value(event.target.value)}
 />*/}
-          <input
-            className="frame2-group1-2"
-            placeholder="Contraseña"
-            type="password"
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-          />
+              <input
+                className="frame2-2-1-1-1"
+                placeholder="Contraseña"
+                type="password"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+              />
+            </div>
+            <button className="frame2-2-1-2">
+              <div className="frame2-2-1-2-text">Iniciar sesión</div>
+            </button>
+            {/* Mensajes de error o éxito */}
+            {errorMessage && (
+              <div className="error-message">{errorMessage}</div>
+            )}
+            {successMessage && (
+              <div className="success-message">{successMessage}</div>
+            )}
+          </form>
         </div>
-        <button className="frame2-group2">
-          <div className="frame2-group2-rectangle" />
-          <div className="frame2-group2-iniciar-sesion">Iniciar sesión</div>
-        </button>
-        {/* Mensajes de error o éxito */}
-        {errorMessage && <div className="error-message">{errorMessage}</div>}
-        {successMessage && (
-          <div className="success-message">{successMessage}</div>
-        )}
-      </form>
+      </div>
+      <div className="frame1">
+        <img
+          className="logo-combined-191a1e-icon"
+          alt=""
+          src="/logo-combined-191a1e@2x.png"
+        />
+        <img className="sits-01-icon" alt="" src="/sits-01@2x.png" />
+      </div>
     </div>
   );
 };
--- frontend/src/pages/RegisterPage.js
+++ frontend/src/pages/RegisterPage.js
@@ -47,86 +47,93 @@ const RegisterPage = () => {
     }
   };
 
+  const handleNavigate = () => {
+    navigate("/login");
+  };
+
   return (
     <div className="register">
-      <img className="sits-01-icon" alt="" src="/sits-01@2x.png" />
-      <img
-        className="logo-combined-191a1e-icon"
-        alt=""
-        src="/logo-combined-191a1e@2x.png"
-      />
-
-      <div className="frame1">
-        <div className="frame1-1">
-          <h1 className="frame1-1-registrate">Regístrate</h1>
-          <h1 className="frame1-1-introduciendo-tus-dat">
-            introduciendo tus datos
-          </h1>
-        </div>
-        <div className="frame1-2">
-          <div className="frame1-2-si-ya-tienes-una-cuen">
-            Si ya tienes una cuenta inicia sesión
+      <div className="frame2">
+        <div className="frame2-1">
+          <div className="frame2-1-1">
+            <h1 className="frame2-1-1-text1">Regístrate</h1>
+            <h1 className="frame2-1-1-text2">introduciendo tus datos</h1>
           </div>
-          <a className="frame1-2-puedes-iniciar-sesion">
-            <span>
-              <span>¡Puedes</span>
-              <span className="span">{` `}</span>
-            </span>
-            <span className="iniciar-sesin-aqu">iniciar sesión aquí!</span>
-          </a>
+          <div className="frame2-1-2">
+            <h1 className="frame2-1-2-text1">
+              Si ya tienes una cuenta inicia sesión
+            </h1>
+            <a className="frame2-1-2-text2" onClick={handleNavigate}>
+              <span>
+                <span>¡Puedes</span>
+                <span className="span">{` `}</span>
+              </span>
+              <span className="iniciar-sesin-aqu">iniciar sesión aquí!</span>
+            </a>
+          </div>
+        </div>
+        <div className="frame2-2">
+          <h1 className="frame2-2-text">Regístrate</h1>
+          {/* Formulario de registro */}
+          <form className="frame2-2-1" onSubmit={handleSubmit}>
+            <div className="frame2-2-1-1">
+              <input
+                className="frame2-2-1-1-1"
+                placeholder="Correo electrónico"
+                type="email"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+              />
+              <input
+                className="frame2-2-1-1-1"
+                placeholder="Nombre de usuario"
+                type="text"
+                value={username}
+                onChange={(event) => setUsername(event.target.value)}
+              />
+              <input
+                className="frame2-2-1-1-1"
+                placeholder="Nombre completo"
+                type="text"
+                value={completeName}
+                onChange={(event) => setCompleteName(event.target.value)}
+              />
+              <input
+                className="frame2-2-1-1-1"
+                placeholder="Contraseña"
+                type="password"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+              />
+              <input
+                className="frame2-2-1-1-1"
+                placeholder="Confirmar contraseña"
+                type="password"
+                value={confirmPassword}
+                onChange={(event) => setConfirmPassword(event.target.value)}
+              />
+            </div>
+            <button className="frame2-2-1-2">
+              <div className="frame2-2-1-2-text">Registrarse</div>
+            </button>
+            {/* Mensajes de error o éxito */}
+            {errorMessage && (
+              <div className="error-message">{errorMessage}</div>
+            )}
+            {successMessage && (
+              <div className="success-message">{successMessage}</div>
+            )}
+          </form>
         </div>
       </div>
-
-      {/* Formulario de registro */}
-      <form onSubmit={handleSubmit} className="frame2">
-        <h1 className="frame2-registrate">Regístrate</h1>
-        <div className="frame2-group1">
-          <input
-            className="frame2-group1-1"
-            placeholder="Correo electrónico"
-            type="email"
-            value={email}
-            onChange={(event) => setEmail(event.target.value)}
-          />
-          <input
-            className="frame2-group1-2"
-            placeholder="Nombre de usuario"
-            type="text"
-            value={username}
-            onChange={(event) => setUsername(event.target.value)}
-          />
-          <input
-            className="frame2-group1-3"
-            placeholder="Nombre completo"
-            type="text"
-            value={completeName}
-            onChange={(event) => setCompleteName(event.target.value)}
-          />
-          <input
-            className="frame2-group1-4"
-            placeholder="Contraseña"
-            type="password"
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-          />
-          <input
-            className="frame2-group1-5"
-            placeholder="Confirmar contraseña"
-            type="password"
-            value={confirmPassword}
-            onChange={(event) => setConfirmPassword(event.target.value)}
-          />
-        </div>
-        <button className="frame2-group2">
-          <div className="frame2-group2-rectangle" />
-          <div className="frame2-group2-registrarse">Registrarse</div>
-        </button>
-        {/* Mensajes de error o éxito */}
-        {errorMessage && <div className="error-message">{errorMessage}</div>}
-        {successMessage && (
-          <div className="success-message">{successMessage}</div>
-        )}
-      </form>
+      <div className="frame1">
+        <img
+          className="logo-combined-191a1e-icon"
+          alt=""
+          src="/logo-combined-191a1e@2x.png"
+        />
+        <img className="sits-01-icon" alt="" src="/sits-01@2x.png" />
+      </div>
     </div>
   );
 };
